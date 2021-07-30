2ca66b49337ea3211fdb0f997c9e18aa312587a8
YiSuHo <xon235@naver.com>
2021-07-30 17:47:34 +0900

    Implement Device List Logging

--- app/src/main/java/com/yisuho/wiredtowireless/MainActivity.kt
+++ app/src/main/java/com/yisuho/wiredtowireless/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.yisuho.wiredtowireless
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val textView: TextView by lazy { findViewById(R.id.textView) }
+    private val manager: UsbManager by lazy { getSystemService(Context.USB_SERVICE) as UsbManager }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onRefreshDeviceList(view: View) {
+
+        textView.text = "USB Device List"
+        var index = 0
+        manager.deviceList.forEach{
+            textView.append("Entry $index: ${it.key}, ${it.value}")
+            index += 1
+        }
     }
 
     fun onOpenLogging(view: View) {
